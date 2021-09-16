20eb47a322554d2d2442ed19cb4436c18c2202e4
khalila <hel000941@gmail.com>
2021-09-16 11:44:50 +0700

    handle click menu item

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,6 @@
+import 'package:dindag/ui/menu/laporan.dart';
+import 'package:dindag/ui/menu/perusahaan.dart';
+import 'package:dindag/ui/menu/tambah_laporan.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -30,8 +33,7 @@ class _HomeState extends State<Home> {
                   ),
                   CircleAvatar(
                     radius: 30,
-                    backgroundImage:
-                        AssetImage("assets/img/default/avatar.png"),
+                    backgroundImage: AssetImage("assets/img/default/avatar.png"),
                   )
                 ],
               ),
@@ -62,82 +64,109 @@ class _HomeState extends State<Home> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    height: 150,
-                    padding: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Colors.blue[700],
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                    child: Column(
-                      //TODO: Temporary, until the image is available
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        //TODO: Add Image
-                        Text(
-                          "Perusahaan",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13,
-                          ),
-                          textAlign: TextAlign.center,
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Perusahaan(),
                         ),
-                      ],
+                      );
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      height: 150,
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.blue[700],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: Column(
+                        //TODO: Temporary, until the image is available
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          //TODO: Add Image
+                          Text(
+                            "Perusahaan",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    height: 150,
-                    padding: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Colors.blue[700],
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                    child: Column(
-                      //TODO: Temporary, until the image is available
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        //TODO: Add Image
-                        Text(
-                          "Laporan",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13,
-                          ),
-                          textAlign: TextAlign.center,
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Laporan(),
                         ),
-                      ],
+                      );
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      height: 150,
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.blue[700],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: Column(
+                        //TODO: Temporary, until the image is available
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          //TODO: Add Image
+                          Text(
+                            "Laporan",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    height: 150,
-                    padding: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Colors.blue[700],
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                    child: Column(
-                      //TODO: Temporary, until the image is available
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        //TODO: Add Image
-                        Text(
-                          "Buat/Tambah Laporan",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13,
-                          ),
-                          textAlign: TextAlign.center,
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => TambahLaporan(),
                         ),
-                      ],
+                      );
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      height: 150,
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.blue[700],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: Column(
+                        //TODO: Temporary, until the image is available
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          //TODO: Add Image
+                          Text(
+                            "Buat/Tambah Laporan",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
